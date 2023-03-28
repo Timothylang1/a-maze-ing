793db645d1b11cbda3bbd71d33030818d9043d85
Timothylang1 <tlang@macalester.edu>
2023-03-27 19:08:22 -0700

    Added in gradle

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+import edu.macalester.graphics.CanvasWindow;
+
+class Test {
+    CanvasWindow window = new CanvasWindow("Yolo", 100, 100);
+    public Test() {
+
+    }
+
+
+    public static void main(String[] args) {
+        new Test();
+    }
+}
+
